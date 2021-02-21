8a612b9feeb5c61a5dc3f9d8e79ba5317bf04d60
TinyClayMan <tinyclayman@gmail.com>
2021-02-21 17:43:28 +0300

    Correct pattern

--- src/Practice1/Practice1.java
+++ src/Practice1/Practice1.java
@@ -7,9 +7,9 @@ import java.util.regex.Pattern;
 public class Practice1 {
 
     public static void main(String[] args) {
-        final String[] input = {"my@protonmail.su", "a"};
+        final String[] input = {"my@protonmail.su", "a", "inv@.al.id.mail", "uw@uw@u.ru", "another@valid.mail"};
 
-        Predicate<String> check = checkThis -> Pattern.matches(".*@.*", checkThis);
+        Predicate<String> check = checkThis -> Pattern.matches("^[^@]+@[^.][^@]+", checkThis);
         Arrays.stream(input).filter(check).forEach(System.out::println);
     }
 }
